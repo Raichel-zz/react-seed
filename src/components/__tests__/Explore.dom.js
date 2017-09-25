351b83f2009e887775ec543bec3dfcdf622c5571
raichel <eran.raichel@gmail.com>
2017-09-25 16:17:22 +0300

    initial commit

---
+++ src/components/__tests__/Explore.dom.js
@@ -0,0 +1,19 @@
+import 'jsdom-global/register';
+import React from 'react';
+import { mount } from 'enzyme';
+import Explore from "../Explore";
+
+test('Explore changes the text after click', () => {
+  // Render an explore comp
+  let test_value = "initial value";
+  let onClick = (val) => {
+    test_value = val;
+    expect(val).toEqual('blah blah');
+  }
+
+  const explore = mount(<Explore onChange={val => onClick(val)} value={test_value}/>);
+  explore.find('input').node.value = "blah blah";
+  explore.find('button').simulate('click');
+
+
+});
